5e12fbc6e4219c1be5665dee2bb30cf97783f9b3
Alejandro <alekosescu@gmail.com>
2022-03-06 10:57:41 -0300

    Styles

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
-export default function LandingPage() { 
-    return (
-        <div>   
-            <h1>Bienvenue sur ma page de recette!</h1>
-            <Link to='/home'>
-                <button>Entrer dans</button>
-            </Link>
-        </div>
-    )
+export default function LandingPage() {
+  return (
+    <div>
+      <div className='type'>
+        <h1>Bienvenue sur ma page de recette!</h1>
+        <Link to="/home">
+          <button>Entrer dans</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
